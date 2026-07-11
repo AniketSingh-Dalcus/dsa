b1531091c35d796f67cf7807dfbafbb937f9aa27
Aniket Singh <aniketofficially9@gmail.com>
2026-07-11 23:53:57 +0530

    Time: 39 ms (46.58%) | Memory: 13.2 MB (61.78%) - LeetSync

---
+++ 55-jump-game/jump-game.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def canJump(self, nums):
+        max_index = 0  # farthest index we can reach
+        for i in range(len(nums)):
+            if i > max_index:
+                # if we are at an index we cannot reach
+                return False
+            # update the farthest index reachable
+            max_index = max(max_index, i + nums[i])
+
+        return True
